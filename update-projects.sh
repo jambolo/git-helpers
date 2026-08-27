6805f9ab1de6502c57d794cf81d8c0fc47f75abb
John Bolton <jambolo@users.noreply.github.com>
2026-08-27 16:38:55 -0700

    Added --force to allow tag movement. Catch errors so an error doesn't halt all updates.

--- update-projects.sh
+++ update-projects.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-# version: 1.0.0
+# version: 1.1.0
 #
 # Goes into each subdirectory with a Git repo and does a pull and a submodule update.
 
 set -e  # Exit on error
 
 for d in *; do
-        if [ -d "$d" ] && [ "$d" != "3rdParty" ]; then
-                pushd "$d" 1> /dev/null
-                if git rev-parse --git-dir > /dev/null 2>&1; then
-                        echo "> $d"
-                # Fetch all branches and tags from all remotes, prune obsolete branches, and merge the current branch
-                git fetch --all --tags --prune
-                        if git rev-parse @{u} > /dev/null 2>&1; then
-                                git merge "$(git rev-parse --abbrev-ref --symbolic-full-name @{u})"
-                        fi
-                        # Update submodules
-                        git submodule update --recursive
-                        echo ""
-                fi
-                popd 1> /dev/null
-        fi
+  if [ -d "$d" ] && [ "$d" != "3rdParty" ]; then
+    pushd "$d" 1> /dev/null
+    if git rev-parse --git-dir > /dev/null 2>&1; then
+      echo "> $d"
+      # Fetch all branches and tags from all remotes, prune obsolete branches, and merge the current branch
+      git fetch --all --tags --prune --force || echo "warning: fetch failed for $d"
+      if git rev-parse @{u} > /dev/null 2>&1; then
+        git merge "$(git rev-parse --abbrev-ref --symbolic-full-name @{u})"
+      fi
+      # Update submodules
+      git submodule update --recursive
+      echo ""
+    fi
+    popd 1> /dev/null
+  fi
 done
